9178b22e2b659c06f9211cb5662e32312fa56b51
Jerin Joice <jerinjoice18@gmail.com>
2025-12-03 21:14:46 +0530

    Day 2 Completed

--- day2.c
+++ day2.c
@@ -4,18 +4,31 @@
 
 void main(){
     FILE *fp=fopen("day2input.txt","r");
-    long long sum=0, aid,bid,i;
-    int mid;
+    long long sum=0, aid,bid,i,j;
+    int mid,k,len,matchflag,invalidflag;
     char comma,strin[40];
     while(fscanf(fp,"%lld-%lld%c",&aid,&bid,&comma)>=2){
         for(i=aid;i<=bid;i++){
             sprintf(strin,"%lld",i);
-            if(strlen(strin)%2==0){
-                mid=(strlen(strin)/2);
-                if(strncmp(&strin[0],&strin[mid],mid)==0){
-                    sum+=i;
+            len=strlen(strin);
+            invalidflag=0;
+            for(k=1;k<=len/2;k++){
+                if(len%k==0){
+                    matchflag=1;
+                    for(j=k;j<len;j+=k){
+                        if(strncmp(&strin[0],&strin[j],k)!=0){
+                            matchflag=0;
+                            break;
+                        }
+                    }
+                    if(matchflag==1){
+                        invalidflag=1;
+                        break;
+                    }
                 }
-
+            }
+            if(invalidflag==1){
+                sum+=i;
             }
         }
 
