614730b628713729bf32eb2b509dddd0d18b83bb
Jerin Joice <jerinjoice18@gmail.com>
2025-12-05 20:53:18 +0530

    Day 5 Completed

--- day5.c
+++ day5.c
@@ -7,41 +7,49 @@ struct Range {
     long long max;
 };
 
-void main() {
+int main() {
     FILE *fp = fopen("day5input.txt", "r");
-    if (fp == NULL) {
-        printf("File not found\n");
-        return;
-    }
+    if (fp == NULL) return 1;
 
-    struct Range ranges[200];
-    int range_count = 0;
-    long long fresh_count = 0;
+    struct Range ranges[5000];
+    int count = 0;
     char line[100];
+
     while (fgets(line, sizeof(line), fp)) {
         if (strchr(line, '-') != NULL) {
-            sscanf(line, "%lld-%lld", &ranges[range_count].min, &ranges[range_count].max);
-            range_count++;
+            sscanf(line, "%lld-%lld", &ranges[count].min, &ranges[count].max);
+            count++;
         }
-        else if (strlen(line) > 1) {
-            long long id;
-            if (sscanf(line, "%lld", &id) == 1) {
-                
-                int is_fresh = 0;
-                for (int i = 0; i < range_count; i++) {
-                    if (id >= ranges[i].min && id <= ranges[i].max) {
-                        is_fresh = 1;
-                        break;
-                    }
-                }
+    }
 
-                if (is_fresh == 1) {
-                    fresh_count++;
-                }
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = 0; j < count - i - 1; j++) {
+            if (ranges[j].min > ranges[j + 1].min) {
+                struct Range temp = ranges[j];
+                ranges[j] = ranges[j + 1];
+                ranges[j + 1] = temp;
             }
         }
     }
-    printf("Total fresh ingredients: %lld\n", fresh_count);
+    long long total_fresh = 0;
+    long long last_counted_id = -1;
 
+    for (int i = 0; i < count; i++) {
+        long long start = ranges[i].min;
+        long long end = ranges[i].max;
+        if (start <= last_counted_id) {
+            start = last_counted_id + 1;
+        }
+        if (end >= start) {
+            long long new_ids = (end - start) + 1;
+            total_fresh += new_ids;
+            
+            last_counted_id = end;
+        }
+    }
+
+    printf("Total unique fresh ingredients: %lld\n", total_fresh);
+    
     fclose(fp);
+    return 0;
 }
