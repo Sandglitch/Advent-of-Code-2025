4381c90135d7d5d5146a7c6359af09e65be874da
Jerin Joice <jerinjoice18@gmail.com>
2025-12-11 20:49:13 +0530

    Completed Day 8

--- day8.c
+++ day8.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 #define MAX_BOXES 2000
-#define MAX_EDGES 2000000 
+#define MAX_EDGES 2000000
 
 typedef struct {
     int id;
@@ -18,8 +18,6 @@ typedef struct {
 Point boxes[MAX_BOXES];
 Edge edges[MAX_EDGES];
 int parent[MAX_BOXES];
-int sz[MAX_BOXES];
-
 
 double get_dist(Point a, Point b) {
     long long dx = a.x - b.x;
@@ -27,7 +25,6 @@ double get_dist(Point a, Point b) {
     long long dz = a.z - b.z;
     return sqrt(dx*dx + dy*dy + dz*dz);
 }
-
 
 int compareEdges(const void *a, const void *b) {
     Edge *e1 = (Edge *)a;
@@ -37,26 +34,20 @@ int compareEdges(const void *a, const void *b) {
     return 0;
 }
 
-
-int compareSizes(const void *a, const void *b) {
-    return (*(int*)b - *(int*)a);
-}
-
-
 int find(int i) {
     if (parent[i] == i)
         return i;
-    return parent[i] = find(parent[i]); 
+    return parent[i] = find(parent[i]);
 }
 
-void unite(int i, int j) {
+int unite(int i, int j) {
     int root_i = find(i);
     int root_j = find(j);
     if (root_i != root_j) {
         parent[root_i] = root_j;
-        sz[root_j] += sz[root_i];
-        sz[root_i] = 0; 
+        return 1;
     }
+    return 0;
 }
 
 int main() {
@@ -72,7 +63,6 @@ int main() {
 
     for (int i = 0; i < box_count; i++) {
         parent[i] = i;
-        sz[i] = 1;
     }
 
     int edge_count = 0;
@@ -87,28 +77,26 @@ int main() {
 
     qsort(edges, edge_count, sizeof(Edge), compareEdges);
 
-    int limit = 1000;
-    if (edge_count < 1000) limit = edge_count; 
+    int successful_merges = 0;
+    long long answer = 0;
 
-    for (int i = 0; i < limit; i++) {
-        unite(edges[i].u, edges[i].v);
-    }
+    for (int i = 0; i < edge_count; i++) {
+        int u = edges[i].u;
+        int v = edges[i].v;
 
-    int final_sizes[MAX_BOXES];
-    int circuit_count = 0;
+        if (unite(u, v)) {
+            successful_merges++;
 
-    for (int i = 0; i < box_count; i++) {
-        if (parent[i] == i) {
-            final_sizes[circuit_count++] = sz[i];
+            if (successful_merges == box_count - 1) {
+                answer = boxes[u].x * boxes[v].x;
+                printf("Last connection between Box %d (X=%lld) and Box %d (X=%lld)\n", 
+                       u, boxes[u].x, v, boxes[v].x);
+                break;
+            }
         }
     }
 
-    qsort(final_sizes, circuit_count, sizeof(int), compareSizes);
-
-    long long result = (long long)final_sizes[0] * (long long)final_sizes[1] * (long long)final_sizes[2];
-
-    printf("Top 3 circuit sizes: %d, %d, %d\n", final_sizes[0], final_sizes[1], final_sizes[2]);
-    printf("Answer: %lld\n", result);
+    printf("Product of X coordinates: %lld\n", answer);
 
     return 0;
 }
